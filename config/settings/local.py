4784fee2a77fb569b06f9feb4c1aa244f24b5c54
Gambit9111 <bitgam911@gmail.com>
2022-09-08 11:45:00 +0000

    pull this to quickstart any project

---
+++ config/settings/local.py
@@ -0,0 +1,12 @@
+from .base import *  # noqa
+from .base import env
+
+DEBUG = True
+
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+)
+
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+
+ADMIN_URL = "admin/"
