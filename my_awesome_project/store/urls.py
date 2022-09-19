8a1ed568617e876192892667a27703e666cca387
Gambit9111 <bitgam911@gmail.com>
2022-09-19 08:42:24 +0000

    product_detail view done

--- my_awesome_project/store/urls.py
+++ my_awesome_project/store/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import all_categories
+from .views import all_categories, products_by_category, product_detail, cart_view
 
 app_name = "store"
 urlpatterns = [
     path("", all_categories, name="all_categories"),
+    path("cart/", cart_view, name="cart_view"),
+    path('<slug:slug_cat>/', products_by_category, name="products_by_category"),
+    path('<slug:slug_cat>/<slug:slug_prod>/', product_detail, name="product_detail"),
 ]
--- my_awesome_project/store/views.py
+++ my_awesome_project/store/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
-from .models import Category
+from .models import Category, Product
 
 # categories view
 def all_categories(request):
     categories = Category.get_all_categories()
     return render(request, "store/all_categories.html", {"categories": categories})
+
+def products_by_category(request, slug_cat):
+    products = Product.get_all_products_by_category_slug(slug_cat)
+    return render(request, "store/products_by_category.html", {"products": products})
+
+def product_detail(request, slug_cat, slug_prod):
+    product = Product.objects.get(slug=slug_prod)
+    device = request.COOKIES['device']
+
+    if request.method == 'POST':
+        quantity = request.POST['quantity']
+        print(quantity)
+        print(device)
+    return render(request, "store/product_detail.html", {"product": product})
+
+def cart_view(request):
+    return render(request, "store/cart.html")
